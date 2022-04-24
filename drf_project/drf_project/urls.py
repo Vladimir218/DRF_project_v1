35491bf37f5a06d3ef6d9f4cfa410effe5e61977
Vladimir <kvv23@tut.by>
2022-04-25 00:10:18 +0300

    Создаем Routers

--- drf_project/drf_project/urls.py
+++ drf_project/drf_project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
+from users.views import CustomUserModelViewSet
+
+router = DefaultRouter()
+router.register("users", CustomUserModelViewSet)
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-users/', include('rest_framework.urls'))
+    path("admin/", admin.site.urls),
+    path("api-auth", include("rest_framework.urls")),
+    path("api/", include(router.urls)),
 ]
