073bae8bdf4d033ba0900d46d95fb335f184b14d
Vladimir <kvv23@tut.by>
2022-04-25 23:18:43 +0300

    Реформат кода

--- drf_project/users/serializers.py
+++ drf_project/users/serializers.py
@@ -6,9 +6,4 @@ from .models import CustomUser
 class CustomUserModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = CustomUser
-        fields = [
-            "username",
-            "first_name",
-            "last_name",
-            "email",
-        ]
+        fields = ["username", "first_name", "last_name", "email"]
