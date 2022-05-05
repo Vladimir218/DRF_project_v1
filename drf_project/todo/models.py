bbb42d73abe2a8e5ecfe7892bb5c9b0c59fed653
Vladimir <kvv23@tut.by>
2022-05-05 19:25:55 +0300

    lesson3:В приложение todo добавлена модель Project

--- drf_project/todo/models.py
+++ drf_project/todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from users.models import CustomUser
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    repository = models.URLField(blank=True)
+    users = models.ManyToManyField(CustomUser)
+    
+
+    def __str__(self):
+        return self.name
