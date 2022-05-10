65a3b1175893dfdb81db3ae1395824d9fb041f04
Vladimir <kvv23@tut.by>
2022-05-10 22:38:18 +0300

    lesson3:Настроен renderer

--- drf_project/todo/serializers.py
+++ drf_project/todo/serializers.py
@@ -1,16 +1,21 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Project, ToDo
+from users.serializers import CustomUserModelSerializer
 
 
 class ProjectModelSerializer(ModelSerializer):
-
+    
+    users=CustomUserModelSerializer(many=True)
+    
     class Meta:
         model = Project
         fields = '__all__'
 
 
 class ToDoModelSerializer(ModelSerializer):
-
+    
+    creator_user=CustomUserModelSerializer()
+    
     class Meta:
         model = ToDo
         exclude = ('flag_is_active',)
