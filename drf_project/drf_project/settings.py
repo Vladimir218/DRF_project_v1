138f7492f6ad93c0cbd35dac92dfcad4e82e4136
Vladimir <kvv23@tut.by>
2022-05-11 23:38:54 +0300

    lesson4:Пропиан класс для постраничного вывода и установлено кол-во з записей на стронице для проекта - 2

--- drf_project/drf_project/settings.py
+++ drf_project/drf_project/settings.py
@@ -131,5 +131,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         #'rest_framework.renderers.AdminRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
 }
