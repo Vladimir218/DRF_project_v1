e302dbca72ae915e763a53ff1f40609572219e4d
Vladimir <kvv23@tut.by>
2022-05-15 23:58:28 +0300

    lesson4:ПрСоздана модель Project для dcвсех типов запросов, 1 проект на странице, фильтрация по совпадению в названии

--- drf_project/todo/views.py
+++ drf_project/todo/views.py
@@ -5,13 +5,23 @@ from rest_framework.viewsets import ModelViewSet
 
 from .models import Project,ToDo
 from .serializers import ProjectModelSerializer,ToDoModelSerializer
+from rest_framework.pagination import PageNumberPagination
 
 # Create your views here.
 
+class ProjectPagination(PageNumberPagination):
+    page_size = 1
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectPagination
+    def get_queryset(self):
+        queryset = Project.objects.all()
+        name = self.request.query_params.get('name', '')
+        if name:
+            queryset = queryset.filter(name__contains=name)
+        return queryset
 
 
 class ToDoModelViewSet(ModelViewSet):
