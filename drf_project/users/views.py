933c863bd7db2e3b378f985715fe6c8cb3f006db
Vladimir <kvv23@tut.by>
2022-05-15 22:15:18 +0300

    lesson4:ПрСоздана модель User для запросов list, retrive, update

--- drf_project/users/views.py
+++ drf_project/users/views.py
@@ -3,9 +3,16 @@ from rest_framework.viewsets import ModelViewSet
 from .models import CustomUser
 from .serializers import CustomUserModelSerializer
 
+from rest_framework import mixins
+from rest_framework import viewsets
+
 # Create your views here.
 
 
-class CustomUserModelViewSet(ModelViewSet):
+class CustomUserModelViewSet(mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  viewsets.GenericViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserModelSerializer
+
