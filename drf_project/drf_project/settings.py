03fc5c399e21826a9fe457f41f6dc343b13a25d2
Vladimir <kvv23@tut.by>
2022-06-05 13:34:36 +0300

    lesson8:С:Создали документацию для API, используя drf-yasg

--- drf_project/drf_project/settings.py
+++ drf_project/drf_project/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
+    'rest_framework',
     'corsheaders',
     'django_filters',
     'rest_framework.authtoken',
+    'drf_yasg',
     "users",
     "todo",  
 ]
--- drf_project/drf_project/urls.py
+++ drf_project/drf_project/urls.py
@@ -20,6 +20,22 @@ from rest_framework.routers import DefaultRouter
 from users.views import CustomUserModelViewSet
 from todo.views import ProjectModelViewSet, ToDoModelViewSet
 from rest_framework.authtoken import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ToDo",
+      default_version='0.1',
+      description="Documentation to out project",
+      contact=openapi.Contact(email="KVV@admin.local"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 router = DefaultRouter()
 router.register("users", CustomUserModelViewSet)
@@ -35,4 +51,7 @@ urlpatterns = [
     #re_path(r'^api/(?P<version>\d\.\d)/users/$', CustomUserModelViewSet.as_view({'get': 'list'})), #UrlPathVersioning
     path("api/", include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
